4411246118acb8b631e2d5f40fd239c8ca711005
Geetesh Gupta <gupta.15@iitj.ac.in>
2021-03-02 22:15:24 +0530

    Add react-bootstrap and create basic ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
-import * as React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import * as React from 'react';
+import './App.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { FaCloud, FaRocket } from 'react-icons/fa';
+import { AiOutlineDeploymentUnit } from 'react-icons/ai';
 
 const App = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [activeNavKey, setActiveNavKey] = React.useState('/home');
+	return (
+		<Container fluid className='p-0' style={{ width: '480px', height: '480px' }}>
+			<Row className='justify-content-center m-0 p-2 bg-primary text-light'>
+				<h4 className='header'>AIO Cloud Management</h4>
+			</Row>
+			<Row className='m-0 h-100'>
+				<div className='p-0 bg-secondary justify-content-center align-items-center flex-column'>
+					<Nav
+						variant='pills'
+						activeKey={activeNavKey}
+						onSelect={(selectedKey) => setActiveNavKey(selectedKey)}
+						className='flex-column p-0'>
+						<Nav.Item>
+							<Nav.Link eventKey='/home'>
+								<FaCloud />
+							</Nav.Link>
+						</Nav.Item>
+						<Nav.Item>
+							<Nav.Link eventKey='/deployments'>
+								<FaRocket />
+							</Nav.Link>
+						</Nav.Item>
+						<Nav.Item>
+							<Nav.Link eventKey='/pods'>
+								<AiOutlineDeploymentUnit />
+							</Nav.Link>
+						</Nav.Item>
+					</Nav>
+				</div>
+				<Col className='bg-light'>
+					<Container className='flex-column py-4'>
+						<h3>Cloud Deployment</h3>
+						<h3>Cloud Deployment</h3>
+						<h3>Cloud Deployment</h3>
+						<h3>Cloud Deployment</h3>
+					</Container>
+				</Col>
+			</Row>
+		</Container>
+	);
 };
 
 export default App;
--- src/popup.tsx
+++ src/popup.tsx
@@ -3,6 +3,7 @@ import * as ReactDOM from "react-dom";
 
 import App from "./App";
 import "./popup.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 var mountNode = document.getElementById("popup");
 ReactDOM.render(<App />, mountNode);
