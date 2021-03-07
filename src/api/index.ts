e3e102316b79b1f41827140cb514cf0348d27168
Anshul Ahuja <anshul.ahu@gmail.com>
2021-03-07 19:37:35 +0530

    Add comments instructions

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,13 +2,14 @@ export const getKubernetesAPIs = (namespace?: string) => {
 	const rootAPI = '/api/v1';
 	const namespaceAPI = `/apis/apps/v1/namespaces/${namespace}`;
 	return {
-		DEPLOYMENT_LIST_API: `${namespaceAPI}/deployments`,
-		STATEFUL_SET_LIST_API: `${namespaceAPI}/statefulsets`,
-		DAEMON_SET_LIST_API: `${namespaceAPI}/daemonsets`,
-		POD_LIST_API: `${namespaceAPI}/pods`,
-		SERVICE_LIST_API: `${namespaceAPI}/services`,
-		NODE_LIST_API: `${rootAPI}/nodes`,
-		EVENT_LIST_API: `${rootAPI}/events`,
-		NAMESPACE_LIST_API: `${rootAPI}/namespaces`,
+		DEPLOYMENT_LIST_API: `${namespaceAPI}/deployments`,  	//Type 1
+		STATEFUL_SET_LIST_API: `${namespaceAPI}/statefulsets`,  //Type 1
+		DAEMON_SET_LIST_API: `${namespaceAPI}/daemonsets`,      //Type 1
+		// Replica Set
+		POD_LIST_API: `${namespaceAPI}/pods`,                   //Type 1
+		SERVICE_LIST_API: `${namespaceAPI}/services`,			//Type 2
+		NODE_LIST_API: `${rootAPI}/nodes`,						//Type 2
+		EVENT_LIST_API: `${rootAPI}/events`,					//Ignore for now
+		NAMESPACE_LIST_API: `${rootAPI}/namespaces`,			//Fetch namespaces initially, no need to list separately
 	};
 };
