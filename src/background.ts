c5ee039a57452f516c9dc20ae49b7373c65788e6
Geetesh Gupta <gupta.15@iitj.ac.in>
2021-03-07 23:40:21 +0530

    Removed unneeded code in background.ts

--- src/background.ts
+++ src/background.ts
@@ -1,15 +1,1 @@
-import { callAPI } from './utils';
-import { getKubernetesAPIs } from './api';
-import { KubernetesAPITypes } from './api/types';
-
 console.log('Hello from background script!');
-
-const kubernetesAPIs = getKubernetesAPIs('kube-system');
-
-callAPI(kubernetesAPIs.NAMESPACE_LIST_API)
-	.then((data: KubernetesAPITypes['NAMESPACE_LIST_API']) => {
-		console.log(data);
-	})
-	.catch((err) => {
-		console.error({ message: 'Problem with calling API', error: err });
-	});
