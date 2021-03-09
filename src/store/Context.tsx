757c43d78d8b5a6f5950f1745a8a221c863a7c9f
Geetesh Gupta <gupta.15@iitj.ac.in>
2021-03-09 11:12:05 +0530

    Add types to Context

--- src/store/Context.tsx
+++ src/store/Context.tsx
@@ -1,22 +1,23 @@
 import * as React from 'react';
-// import { KubernetesAPITypes } from '../api/types';
-// import { V1Namespace } from '../types/api';
+import { NAV_ITEMS } from '../components/navItems';
+import { V1Namespace } from '../types/api';
 
-// const defaultContext: {
-//     namespaceList: V1Namespace[],
-//     activeNamespace: V1Namespace,
-//     loading: boolean,
-//     kubernetesAPIs: KubernetesAPITypes,
-//     setActiveNamespace: (V1Namespace) => void,
-//     updateNamespaceList: () => void,
-// } = {
-//     namespaceList: [],
-//     activeNamespace: null,
-//     loading: false,
-//     kubernetesAPIs: null,
-//     setActiveNamespace: null,
-//     updateNamespaceList: null
-
-// }
-
-export const Context = React.createContext(null)
+export const Context = React.createContext<{
+	namespaceList: V1Namespace[];
+	activeNamespace: V1Namespace;
+	loading: boolean;
+	kubernetesAPIs: {
+		DEPLOYMENT_LIST_API: string;
+		STATEFUL_SET_LIST_API: string;
+		DAEMON_SET_LIST_API: string;
+		POD_LIST_API: string;
+		SERVICE_LIST_API: string;
+		NODE_LIST_API: string;
+		EVENT_LIST_API: string;
+		NAMESPACE_LIST_API: string;
+	};
+	activeNavEventKey: string;
+	setActiveNavEventKey: (eventKey: NAV_ITEMS) => void;
+	setActiveNamespace: (namespace: V1Namespace) => void;
+	updateNamespaceList: () => void;
+}>(null);
