1720198081a72e9a6121754d7c48f2d7d9154dd4
Geetesh Gupta <gupta.15@iitj.ac.in>
2021-03-13 13:45:07 +0530

    Update DeploymentList.tsx

--- src/components/categories/DeploymentList.tsx
+++ src/components/categories/DeploymentList.tsx
@@ -1,7 +1,47 @@
 import React from 'react';
+import { Col, Container, Dropdown, DropdownButton, Row } from 'react-bootstrap';
+import { useStore } from '../../store/useStore';
+import { V1Deployment, V1DeploymentList } from '../../types/api';
+import ContentList from '../base/ContentList';
+import useAPI from '../hooks/useAPI';
 
-const DeploymentList = () => {
-    return (<>DeploymentList</>)
-}
+const Deployment: React.FC<{ data: V1Deployment }> = ({ data }) => {
+	console.log(data);
+	return (
+		<Container className='p-0'>
+			<Row className='align-items-center'>
+				<Col>{data.metadata.name}</Col>
+				<Col className='text-right'>
+					<DropdownButton title='Labels' id='node-list' variant='secondary' size='sm'>
+						{Object.keys(data.metadata.labels).map((key) => (
+							<Dropdown.Item>
+								{key} : {data.metadata.labels[key]}
+							</Dropdown.Item>
+						))}
+					</DropdownButton>
+				</Col>
+			</Row>
+		</Container>
+	);
+};
+
+const DeploymentList: React.FC = () => {
+	const { kubernetesAPIs } = useStore();
+	const deploymentList = useAPI<V1DeploymentList>(kubernetesAPIs.DEPLOYMENT_LIST_API);
+
+	return (
+		<>
+			{deploymentList !== null && deploymentList.items.length > 0 ? (
+				<ContentList
+					list={deploymentList.items}
+					title={'Deployments'}
+					renderItem={({ data }) => <Deployment data={data} />}
+				/>
+			) : (
+				'Loading...'
+			)}
+		</>
+	);
+};
 
 export default DeploymentList;
