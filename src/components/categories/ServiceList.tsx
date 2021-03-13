15af3e88793656dbf6c6f4e4069463f6a4e016bd
Geetesh Gupta <gupta.15@iitj.ac.in>
2021-03-13 14:09:07 +0530

    Update ServiceList.tsx

--- src/components/categories/ServiceList.tsx
+++ src/components/categories/ServiceList.tsx
@@ -1,7 +1,46 @@
 import React from 'react';
+import { Col, Container, Dropdown, DropdownButton, Row } from 'react-bootstrap';
+import { useStore } from '../../store/useStore';
+import { V1Service, V1ServiceList } from '../../types/api';
+import ContentList from '../base/ContentList';
+import useAPI from '../hooks/useAPI';
 
-const ServiceList = () => {
-    return (<>ServiceList</>)
-}
+const Service: React.FC<{ data: V1Service }> = ({ data }) => {
+	return (
+		<Container className='p-0'>
+			<Row className='align-items-center'>
+				<Col>{data.metadata.name}</Col>
+				<Col className='text-right'>
+					<DropdownButton title='Labels' id='node-list' variant='secondary' size='sm'>
+						{Object.keys(data.metadata.labels).map((key) => (
+							<Dropdown.Item>
+								{key} : {data.metadata.labels[key]}
+							</Dropdown.Item>
+						))}
+					</DropdownButton>
+				</Col>
+			</Row>
+		</Container>
+	);
+};
+
+const ServiceList: React.FC = () => {
+	const { kubernetesAPIs } = useStore();
+	const services = useAPI<V1ServiceList>(kubernetesAPIs.SERVICE_LIST_API);
+
+	return (
+		<>
+			{services !== null && services.items.length >= 0 ? (
+				<ContentList
+					list={services.items}
+					title={'Services'}
+					renderItem={({ data }) => <Service data={data} />}
+				/>
+			) : (
+				'Loading...'
+			)}
+		</>
+	);
+};
 
 export default ServiceList;
