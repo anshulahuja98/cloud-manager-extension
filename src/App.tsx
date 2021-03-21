58c01e47ba3d28c1d1315705746764be31d2dfe0
Geetesh Gupta <gupta.15@iitj.ac.in>
2021-03-22 01:06:12 +0530

    Store kubeconfig.yaml content in cookies

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Navbar from './components/Navbar';
 import ContentContainer from './components/ContentContainer';
 import TopBar from './components/TopBar';
 import { useStore } from './store/useStore';
+import { CookiesProvider } from 'react-cookie';
 
 const App = () => {
 	const { updateNamespaceList, activeNavEventKey, setActiveNavEventKey } = useStore();
@@ -16,17 +17,19 @@ const App = () => {
 	}, []);
 
 	return (
-		<Container fluid className='p-0' style={{ width: '480px', height: '360px' }}>
-			<TopBar />
-			<Row className='m-0 h-100'>
-				<div className='p-0 justify-content-center align-items-center flex-column navbar-container'>
-					<Navbar activeNavEventKey={activeNavEventKey} setActiveNavEventKey={setActiveNavEventKey} />
-				</div>
-				<Col className='pb-2 pr-2 pl-3'>
-					<ContentContainer activeNavEventKey={activeNavEventKey} />
-				</Col>
-			</Row>
-		</Container>
+		<CookiesProvider>
+			<Container fluid className='p-0' style={{ width: '480px', height: '360px' }}>
+				<TopBar />
+				<Row className='m-0 h-100'>
+					<div className='p-0 justify-content-center align-items-center flex-column navbar-container'>
+						<Navbar activeNavEventKey={activeNavEventKey} setActiveNavEventKey={setActiveNavEventKey} />
+					</div>
+					<Col className='pb-2 pr-2 pl-3'>
+						<ContentContainer activeNavEventKey={activeNavEventKey} />
+					</Col>
+				</Row>
+			</Container>
+		</CookiesProvider>
 	);
 };
 
--- src/components/categories/Settings.tsx
+++ src/components/categories/Settings.tsx
@@ -2,17 +2,45 @@ import React from 'react';
 import { Form } from 'react-bootstrap';
 import { useStore } from '../../store/useStore';
 import yaml from 'js-yaml';
+import { useCookies } from 'react-cookie';
 
 const Settings = () => {
 	const { config, setConfig } = useStore();
 	let fileReader = null;
+	const [cookies, setCookie, removeCookie] = useCookies(['kubeconfig']);
+
+	const resetCookies = () => {
+		Object.keys(cookies).map((cookie) => {
+			removeCookie(cookie);
+		});
+	};
+
+	const handleSaveConfigCookie = (config) => {
+		const contextNames = [];
+		config.contexts.forEach((contextObj) => {
+			const context = contextObj.context;
+			const contextName = contextObj.name;
+			const clusterObj = config.clusters.filter((cluster) => cluster.name === context.cluster)[0];
+			const userObj = config.users.filter((user) => user.name === context.user)[0];
+			const contextData = {
+				name: contextName,
+				token: userObj.user.token ? userObj.user.token : null,
+				server: clusterObj.cluster.server,
+			};
+			contextNames.push(contextName);
+			setCookie(contextName, JSON.stringify(contextData), { path: '/' });
+		});
+		setCookie('contexts', JSON.stringify(contextNames), { path: '/' });
+		setCookie('current-context', config['current-context'], { path: '/' });
+		setCookie('apiVersion', config['apiVersion'], { path: '/' });
+	};
 
 	const handleFileRead = (e) => {
-		const content = fileReader.result;
+		const yamlCcontent = fileReader.result;
 		try {
-			let data = yaml.load(content);
-			console.log(data);
-			setConfig(data);
+			let jsonContent = yaml.load(yamlCcontent);
+			setConfig(jsonContent);
+			handleSaveConfigCookie(jsonContent);
 		} catch (e) {
 			console.log(e);
 		}
@@ -25,6 +53,7 @@ const Settings = () => {
 	};
 
 	const handleConfigFileUpload = (e) => {
+		resetCookies();
 		const file = e.target.files[0];
 		handleFileChosen(file);
 	};
